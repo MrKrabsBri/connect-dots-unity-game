50c6fef1265352109ab8a69769c65fe568febbb3
MrKrabsBri <julius.bri@outlook.com>
2024-05-15 13:08:47 +0300

    fix: game finished, code cleanup 1.2

--- Assets/Scripts/Point.cs
+++ Assets/Scripts/Point.cs
@@ -1,59 +1,16 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
-public class Point : Collidable, ClickInterface {
+// Class to represent a point with x and y coordinates
+[System.Serializable]
+public class Point {
+    public float? x { get; set; }
+    public float? y { get; set; }
 
-    public Sprite checkedPoint;
-
-/*    private void OnTriggerEnter2D(Collider2D other) {
-       // OnCollide(other);
-    }*/
-
-    protected override void Update() {
-        ButtonClicked(0);
+    public Point(float? x, float? y) {
+        this.x = x;
+        this.y = y;
     }
 
-    /*    protected override void OnCollide(Collider2D coll) {
-            Debug.Log("Collided with " + coll.name);
-            if (coll.CompareTag("Point")) {
-                Debug.Log("changing color");
-                SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
-                spriteRenderer.sprite = checkedPoint;
-            }
-        }*/
-
-    // This method is called when another collider enters the trigger collider attached to this GameObject
-    private void OnTriggerEnter2D(Collider2D other) {
-        Debug.Log("Collision detected with: " + other.gameObject.name);
-
-        // You can add more logic here to handle the collision
-    }
-
-
-    public void ButtonClicked(int button) {
-        if (Input.GetMouseButtonDown(button)) {
-            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            RaycastHit2D hit = Physics2D.Raycast(mousePosition, Vector2.zero);
-
-            if (hit.collider != null) {
-                Debug.Log("Clicked on: " + hit.collider.gameObject.name);
-
-                if (hit.collider.gameObject.tag == "Point") {
-                    hit.collider.gameObject.GetComponent<SpriteRenderer>().sprite =
-                        checkedPoint;
-                }
-
-
-            }
-        }
-    }
 }
-
-
-
-
-
-
